8f1ddf9913b05c9e320edbad18e09c94d8b4745c
Kartik Kannapur <kartikkannapur@gmail.com>
2015-09-29 16:50:15 +0530

    RESTful APIs with Flask

---
+++ REST_API_with_Flask/01_minimal_restful_api.py
@@ -0,0 +1,26 @@
+__author__ = "Kartik Kannapur"
+
+"""
+Flask-RESTful API 
+"""
+
+# #Python Library Imports
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
---
+++ REST_API_with_Flask/README.md
@@ -0,0 +1,11 @@
+## REST APIs in Python
+----------------------
+
+REST : REpresentation State Transfer
+curl, urllib2, requests
+
+#### Flask-RESTful
+
+Flask-RESTful is an extension of Flask that adds support for quickly building REST APIs.
+
+    $ pip install flask-restful
